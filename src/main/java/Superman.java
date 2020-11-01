8e942911f776d52cfac7e96a97465082781e231b
suguz06 <68874870+suguz06@users.noreply.github.com>
2020-11-01 09:24:09 -0600

    first commit today

---
+++ src/main/java/Superman.java
@@ -0,0 +1,8 @@
+public class Superman {
+
+    public static void main(String[] args) {
+
+        System.out.println("Superman is loading:)");
+
+    }
+}
---
+++ src/test/java/Batman.java
@@ -0,0 +1,7 @@
+public class Batman {
+
+    public static void main(String[] args) {
+
+        System.out.println("Batman is loading:)");
+    }
+}
