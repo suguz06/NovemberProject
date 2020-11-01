6d889be9f73fb149a280eccfa06441b966027b04
suguz06 <68874870+suguz06@users.noreply.github.com>
2020-11-01 09:55:34 -0600

    added Joker in Batman

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -8,5 +8,7 @@ public class Batman {
 
         System.out.println("We added gotham branch==>>> " +
                 "now we work on gotham branch..");
+
+        System.out.println("Joker added in Batman");
     }
 }
